ee7995d2da2acf9f8ab09505e0d9ce68c8797851
Ellie Nam <ellienjuly@gmail.com>
2021-11-14 18:26:55 -0500

    Connect database

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
+const app = express();
+const path = require('path');
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const db = mongoose.connection;
@@ -9,14 +12,16 @@ db.once('open', () => {
   console.log('Databse connected');
 })
 
-const app = express();
-const path = require('path');
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/campgrounds', async(req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', {campgrounds})
 })
 
 app.listen(3000, () => {
